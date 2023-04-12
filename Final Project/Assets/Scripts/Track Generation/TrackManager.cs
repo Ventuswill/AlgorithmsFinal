5d69885aa88af19c11cafc8fc237110b453683f4
youn0628 <youn0628@WJ310-03>
2023-04-12 11:28:51 -0400

    Initial Commit - Starter Code

---
+++ Final Project/Assets/Scripts/Track Generation/TrackManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackManager : MonoBehaviour {
+    public Catmul[] splines;
+    public GameObject splinePrefab;
+    public GameObject[] swarmleaderPrefab;
+    public string[][] maskNames;
+
+	// Use this for initialization
+	void Start () {
+        maskNames = new string[5][];
+
+        maskNames[0] = new string[] { "flock1", "flock2", "flock3", "flock4" };
+        maskNames[1] = new string[] { "flock0", "flock2", "flock3", "flock4" };
+        maskNames[2] = new string[] { "flock0", "flock1", "flock3", "flock4" };
+        maskNames[3] = new string[] { "flock0", "flock1", "flock2", "flock4" };
+        maskNames[4] = new string[] { "flock0", "flock1", "flock2", "flock3" };
+        splines = new Catmul[1]; // TODO - change
+        splines[0] = Instantiate(splinePrefab, new Vector3(0, 0, 0), Quaternion.identity).GetComponent<Catmul>();
+        // TODO add code here
+
+        for (int i = 0; i < 1; i++) // TO DO - change code
+        {
+            splines[i].GenerateSpline();
+        }
+        // spawn the flocks on the tracks.  Track 0 is where the player begins.
+        for (int i = 0; i < 1; i++) // TO DO CHANGE CODE 
+        {
+            // TO DO - Spawn the swarm leader
+            // TODO - Get the follow track script, and tell it about the track manager (so it can find more tracks), and the spline.
+            // make sure to set the mask on the flock, and to say which is the player. 
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
